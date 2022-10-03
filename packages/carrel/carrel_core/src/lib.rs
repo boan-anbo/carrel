004987ac3afda30cf6ed85ef340f3353e5219489
boan <gazagoal@gmail.com>
2022-10-03 16:43:29 -0400

    setting up carrel_db package

--- packages/carrel/carrel_core/src/lib.rs
+++ packages/carrel/carrel_core/src/lib.rs
@@ -1,3 +1,7 @@
+//! Core library for carrel.
+//!
+//! At the center of the library, it is the project management and configuration service for Carrel system.
+//! At the periphery, it's connecting all the services, e.g. fireflies, etc to the core project management.
 pub mod fireflies;
 pub mod errors;
 pub mod scaffold;
